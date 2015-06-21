0d1e5a368992ff39bd0ea5fb61f20b22fb237abc
Pav <pav920@gmail.com>
2015-06-20 23:43:16 -0700

    added audio assembling

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from chunk import Chunk
+from chunk import VideoChunk
 import cv2
 cap = cv2.VideoCapture('obama.mp4')
 
@@ -6,9 +6,9 @@ img1= cap.read()[1]
 img2= cap.read()[1]
 
 
-c = Chunk(1200,1240,cap)
-c2 = Chunk(1250,1260,cap)
-c3 = Chunk(1270,1290,cap)
+c = VideoChunk(1200,1240,cap)
+c2 = VideoChunk(1250,1260,cap)
+c3 = VideoChunk(1270,1290,cap)
 print c.getTransitionWeight(c2)
 from helpers import createVideo
 createVideo([c,c2])
