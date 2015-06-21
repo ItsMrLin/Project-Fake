ea5fab27c41cb88c643ebf124642740e5b3aae91
ItsMrLin <JerryLin1206@Gmail.com>
2015-06-20 22:40:27 -0700

    bug fixes

--- video_processing.py
+++ video_processing.py
@@ -1,6 +1,7 @@
 import cv2
+from chunk import Chunk
 
-def getFrameList(filename, timeList):
+def getChunkList(filename, timeList):
     '''
     Given filename and a list fo time info tuple (startTime, endTime, phonemeName),
     return a list of tuple containing (startFrameIndex, endFrameIndex, phonemeName)
@@ -13,8 +14,8 @@ def getFrameList(filename, timeList):
         return
     fps = cap.get(cv2.cv.CV_CAP_PROP_FPS)
 
-    frameList = [None] * len(timeList)
+    chunkList = [None] * len(timeList)
     for i, timeTuple in enumerate(timeList):
-        frameList[i] = (int(timeTuple[0]*fps/1000), int(timeTuple[1]*fps/1000), timeTuple[2])
+        chunkList[i] = Chunk(int(timeTuple[0]*fps/1000), int(timeTuple[1]*fps/1000), cap, timeTuple[2])
 
-    return frameList
+    return chunkList
