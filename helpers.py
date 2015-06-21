7a8a090044ef350aa7e73c7804e73037551e0318
Pav <pav920@gmail.com>
2015-06-20 23:19:09 -0700

    created video assembly and also added gaussian smoothing when binding chunks

--- helpers.py
+++ helpers.py
@@ -1,4 +1,6 @@
 import cv2
+import numpy as np
+from scipy.ndimage.filters import gaussian_filter1d
 
 def createVideo(chunks):
     chunks[0].reset()
@@ -6,11 +8,37 @@ def createVideo(chunks):
     height , width , layers =  ret.shape
     video = cv2.VideoWriter()
     video.open('video.mov',cv2.cv.CV_FOURCC(*'mp4v'),30,(width,height),True)
+    images = []
+    indexes = []
     for chunk in chunks:
         chunk.reset()
+        indexes.append(len(images))
         for i in range(chunk.getStart(),chunk.getEnd()):
             img = chunk.read()
-            video.write(img)
+            images.append(img)
+
+    # Gaussian Smoothing
+
+    for i in range(1,len(indexes)):
+        img_array = np.zeros((height,width,3,8))
+        c = 0
+        for j in range(indexes[i]-4,indexes[i]+4):
+            img_array[:,:,:,c] = images[j]
+            c = c+1
+
+        smoothed = gaussian_filter1d(img_array,sigma=1.5,axis=3)
+        print "smoothed" , smoothed.shape
+        c = 0
+        for j in range(indexes[i]-4,indexes[i]+4):
+            images[j] = smoothed[:,:,:,c].astype("uint8")
+            c = c+1
+
+
+    for img in images:
+        #cv2.imshow('hi',img)
+        video.write(img)
+        #raw_input()
+        print img
 
     cv2.destroyAllWindows()
     video.release()
--- test.py
+++ test.py
@@ -1,8 +1,14 @@
 from chunk import Chunk
 import cv2
 cap = cv2.VideoCapture('obama.mp4')
-c2 = Chunk(1200,1240,cap)
-c = Chunk(0,20,cap)
+
+img1= cap.read()[1]
+img2= cap.read()[1]
+
+
+c = Chunk(1200,1240,cap)
+c2 = Chunk(1250,1260,cap)
+c3 = Chunk(1270,1290,cap)
 print c.getTransitionWeight(c2)
 from helpers import createVideo
 createVideo([c,c2])
